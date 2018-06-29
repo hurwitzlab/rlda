a631b4ef6dc1cb22fc01662ec2825fbdf49b5ce5
Ken Youens-Clark <kyclark@gmail.com>
2018-06-29 09:21:46 -0700

    moar pakcages

--- singularity/install.r
+++ singularity/install.r
@@ -1,1 +1,7 @@
 install.packages('Rcpp');
+install.packages('RcppProgress');
+install.packages('RcppArmadillo');
+install.packages('doParallel')
+install.packages('foreach');
+install.packages('coda');
+install.packages('gtools');
